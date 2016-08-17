5cb9010cdf6b949b5ee6d4594147e817c5160b88
Christopher Hiller <boneskull@boneskull.com>
2016-08-16 22:52:55 -0700

    remove serial runner

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Mocha from 'mocha-core';
 import bdd from 'mocha-ui-bdd';
-import runnerSerial from 'mocha-runner-serial';
 
 export default Mocha({
-  ui: bdd,
-  runner: runnerSerial
+  ui: bdd
 });
